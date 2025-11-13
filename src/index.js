7b10f0fad481ee6eb232bf3139b953b4892e312e
IvanLowLLH <ivanlowlh@gmail.com>
2025-11-13 14:29:59 +0800

    Add ui.js and update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import * as appLogic from './appLogic.js';
+import { loadUI } from "./ui.js";
+
+appLogic.initialiseAppWithDummy();
+loadUI();
---
+++ src/ui.js
@@ -0,0 +1,31 @@
+import * as appLogic from './appLogic.js';
+
+function renderCurrentProjectToDos() {
+    const todoListContainer = document.querySelector('#todo-card-container');
+    todoListContainer.innerHTML = ''; // Clear current list
+
+    // ADD sorting for priority and date
+
+    const toDos = appLogic.getToDosFromCurrentProject();
+    toDos.forEach((toDo, index) => {
+        const toDoCard = document.createElement("div");
+        toDoCard.dataset.id = index;
+        toDoCard.classList.add("toDo-card");
+        
+        // ADD card expansion and delete button
+        toDoCard.innerHTML = `
+            <div class="card-top">
+                <h1 class="toDo-title">${toDo.title}</h1>
+                <p class="toDo-due-date">Due Date: ${toDo.dueDate}</p>
+            </div>
+            <div class="card-bottom">
+                <p>Placeholder for more details.</p>
+            </div>
+        `;
+        todoListContainer.appendChild(toDoCard);
+    })
+};
+
+export function loadUI() {
+    renderCurrentProjectToDos();
+}
